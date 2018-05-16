4f0d495c88114ace0017c83322c5b3a4a5ffc8ef
Bulbizarre <38532335+Bulbizarre@users.noreply.github.com>
2018-05-16 17:20:45 +0200

    textes again

--- python_lanial/progsautdeligne.py
+++ python_lanial/progsautdeligne.py
@@ -6,13 +6,13 @@ Created on Wed May 16 14:24:36 2018
 """
 from os import path as os_path
 PATH = os_path.abspath(os_path.split(__file__)[0])
-monHTML=open("code.txt","r")
+monHTML=open("amour.txt","r")
 data= monHTML.readlines()
 data = [ d[:-2]+"<br>\n" for d in data ]
 monHTML.close()
 #data.replace('\r\n', "<br>")
 #data.replace('\r\n \r\n', "<br>")
-monHTML=open("code2.txt","w")
+monHTML=open("amour2.txt","w")
 for d in data:
     monHTML.write(d)
 monHTML.close()
