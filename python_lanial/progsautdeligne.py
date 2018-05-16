f3136884800deed35d14d8f8fc11d3c3c4e45c64
Bulbizarre <38532335+Bulbizarre@users.noreply.github.com>
2018-05-16 16:42:21 +0200

    pouloulou

---
+++ python_lanial/progsautdeligne.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 16 14:24:36 2018
+
+@author: etudiant
+"""
+from os import path as os_path
+PATH = os_path.abspath(os_path.split(__file__)[0])
+monHTML=open("code.txt","r")
+data= monHTML.readlines()
+data = [ d[:-2]+"<br>\n" for d in data ]
+monHTML.close()
+#data.replace('\r\n', "<br>")
+#data.replace('\r\n \r\n', "<br>")
+monHTML=open("code2.txt","w")
+for d in data:
+    monHTML.write(d)
+monHTML.close()
+print("ok")
